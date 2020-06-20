c17f57054440385fbedc9be2a8088aa13dfaf1ac
vishnu <vishnu31122013@gmail.com>
2020-06-20 13:50:46 +0530

    dd-t1 project created using start.spring.io website

---
+++ code2blog_springboot_dynamodb/src/main/java/com/wordpress/code2blog/code2blog_springboot_dynamodb/Code2blogSpringbootDynamodbApplication.java
@@ -0,0 +1,13 @@
+package com.wordpress.code2blog.code2blog_springboot_dynamodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Code2blogSpringbootDynamodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Code2blogSpringbootDynamodbApplication.class, args);
+	}
+
+}
