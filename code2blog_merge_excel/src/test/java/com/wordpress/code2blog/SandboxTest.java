4c5959a23d28b8e372bd36321ee7bad3c47a2c17
vishnu <vishnu31122013@gmail.com>
2020-08-20 10:52:56 +0530

    me-t1 test successful. Merged two excel files by copying every cell

--- code2blog_merge_excel/src/test/java/com/wordpress/code2blog/SandboxTest.java
+++ code2blog_merge_excel/src/test/java/com/wordpress/code2blog/SandboxTest.java
@@ -1,6 +1,7 @@
 package com.wordpress.code2blog;
 
 import lombok.extern.log4j.Log4j2;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.Test;
 
@@ -20,11 +21,15 @@ public class SandboxTest {
 
         XSSFWorkbook workbook1 = new XSSFWorkbook(excelSource1);
         for (int sheetIndex = 0; sheetIndex < workbook1.getNumberOfSheets(); sheetIndex++) {
-            workbookTarget.createSheet(String.format("%s-%s", excelSource1.getName(), workbook1.getSheetName(sheetIndex)));
+            XSSFSheet oldSheet = workbook1.getSheetAt(sheetIndex);
+            XSSFSheet newSheet = workbookTarget.createSheet(String.format("%s-%s", excelSource1.getName(), workbook1.getSheetName(sheetIndex)));
+            ExcelSheet.copy(oldSheet, newSheet);
         }
         XSSFWorkbook workbook2 = new XSSFWorkbook(excelSource2);
         for (int sheetIndex = 0; sheetIndex < workbook2.getNumberOfSheets(); sheetIndex++) {
-            workbookTarget.createSheet(String.format("%s-%s", excelSource2.getName(), workbook2.getSheetName(sheetIndex)));
+            XSSFSheet oldSheet = workbook2.getSheetAt(sheetIndex);
+            XSSFSheet newSheet = workbookTarget.createSheet(String.format("%s-%s", excelSource2.getName(), workbook2.getSheetName(sheetIndex)));
+            ExcelSheet.copy(oldSheet, newSheet);
         }
 
         FileOutputStream out = new FileOutputStream(excelTarget);
