f7f6658c86f3cd6f7e3ce88d59d96961a8894f34
vishnu <vishnu31122013@gmail.com>
2020-09-18 16:55:46 +0530

    ccs-t1 by using annotation EnableConfigServer, i was able to access config property file from github on web browser

--- code2blog_config_server/src/main/java/com/wordpress/code2blog/code2blog_config_server/Code2blogConfigServerApplication.java
+++ code2blog_config_server/src/main/java/com/wordpress/code2blog/code2blog_config_server/Code2blogConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.wordpress.code2blog.code2blog_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class Code2blogConfigServerApplication {
 
 	public static void main(String[] args) {
